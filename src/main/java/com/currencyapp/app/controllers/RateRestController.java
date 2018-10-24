9be251f58dc2416fd4456b5fd3ddc89511238870
jkloss <j.klos0405@gmail.com>
2018-10-24 23:24:22 +0200

    Working on fixing issues

--- src/main/java/com/currencyapp/app/controllers/RateRestController.java
+++ src/main/java/com/currencyapp/app/controllers/RateRestController.java
@@ -3,6 +3,7 @@ package com.currencyapp.app.controllers;
 import com.currencyapp.app.services.RateService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("http://api.nbp.pl/api/exchangerates/rates")
@@ -13,10 +14,9 @@ public class RateRestController {
         this.rateService = rateService;
     }
 
-    @PostMapping(value = "/{table}/{code}/{startDate}/{endDate}", produces = "application/json")
-    public String processData(@PathVariable String table, @PathVariable String code,
-                              @PathVariable String startDate, @PathVariable String endDate,
-                              Model model) {
+    @PostMapping(value = "/process")
+    public String processData(@RequestParam String table, @RequestParam String code,
+                              @RequestParam String startDate, @RequestParam String endDate) {
 
         return "resultView";
     }
